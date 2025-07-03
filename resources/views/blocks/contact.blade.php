b63602ec492bd6f2504a0833bdd77ccfef2c2d4f
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2025-07-03 10:35:25 +0200

    Automatyczny deploy

--- resources/views/blocks/contact.blade.php
+++ resources/views/blocks/contact.blade.php
@@ -5,7 +5,7 @@ $sectionClass = '';
 <section data-gsap-anim="section" class="contact -spt {{ $sectionClass }}">
 
 	<div class="__wrapper c-main-wide relative z-2">
-		<h2 class="flex __before m-title w-1/2">{!! $g_contact_1['title'] !!}</h2>
+		<h2 class="flex __before m-title w-full md:w-1/2">{!! $g_contact_1['title'] !!}</h2>
 		<div class="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-14">
 			<div class="__content w-full lg:w-11/12 flex flex-col justify-between">
 				<div class="__data">
@@ -28,7 +28,7 @@ $sectionClass = '';
 			</div>
 		</div>
 	</div>
-	<svg class="absolute top-0 right-0 z-1" xmlns="http://www.w3.org/2000/svg" width="928" height="1098" viewBox="0 0 928 1098" fill="none">
+	<svg class="absolute top-0 right-0 z-1 opacity-10 lg:opacity-100" xmlns="http://www.w3.org/2000/svg" width="928" height="1098" viewBox="0 0 928 1098" fill="none">
 		<path fill-rule="evenodd" clip-rule="evenodd" d="M250.351 0C250.351 0 88.4237 151.307 6.10352e-05 233.931L928 1097.67L927.439 531.5L762.568 239.794L896.513 0H250.351Z" fill="#E30613" />
 	</svg>
 </section>
