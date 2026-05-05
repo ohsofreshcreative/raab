23c1e463dae6be04351703458be5eb118aef8b38
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-05-05 15:45:20 +0200

    Automatyczny deploy

--- resources/views/blocks/logos.blade.php
+++ resources/views/blocks/logos.blade.php
@@ -12,20 +12,20 @@ $sectionClass .= ' ' . $background;
 
 <!--- logos -->
 
-<section data-gsap-anim="section" @if(!empty($section_id)) id="{{ $section_id }}" @endif class="b-logos c-main relative -smt {{ $sectionClass }} {{ $section_class }}">
-
-	<div class="__wrapper relative">
-		<h2 data-gsap-element="header" class="__before w-full md:w-1/2">{{ $g_logos['title'] }}</h2>
+<section data-gsap-anim="section" @if(!empty($section_id)) id="{{ $section_id }}" @endif class="b-logos relative -smt {{ $sectionClass }} {{ $section_class }}">
+	<div class="c-main">
+			<div class="__wrapper relative">
+				<h2 data-gsap-element="header" class="__before w-full md:w-1/2">{{ $g_logos['title'] }}</h2>
+			</div>
+		
+			@if (!empty($g_logos['gallery']))
+			<div data-gsap-element="logos" class="__logos grid grid-cols-2 md:grid-cols-4 gap-4 mt-10">
+				@foreach ($g_logos['gallery'] as $image)
+				<div class="__logo relative border b-border-light bg-white flex items-center justify-center h-30 p-8">
+					<img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="max-h-20 w-auto">
+				</div>
+				@endforeach
+			</div>
+			@endif
 	</div>
-
-	@if (!empty($g_logos['gallery']))
-	<div data-gsap-element="logos" class="__logos grid grid-cols-2 md:grid-cols-4 gap-4 mt-10">
-		@foreach ($g_logos['gallery'] as $image)
-		<div class="__logo relative border b-border-light bg-white flex items-center justify-center h-30 p-8">
-			<img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="max-h-20 w-auto">
-		</div>
-		@endforeach
-	</div>
-	@endif
-
 </section>
