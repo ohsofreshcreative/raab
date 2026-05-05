d7c6911697084fbcbe0df551da4c5f827a9b9bad
ohsofreshcreative <132752926+ohsofreshcreative@users.noreply.github.com>
2026-05-05 15:44:10 +0200

    Automatyczny deploy

--- resources/views/blocks/logos.blade.php
+++ resources/views/blocks/logos.blade.php
@@ -14,28 +14,18 @@ $sectionClass .= ' ' . $background;
 
 <section data-gsap-anim="section" @if(!empty($section_id)) id="{{ $section_id }}" @endif class="b-logos c-main relative -smt {{ $sectionClass }} {{ $section_class }}">
 
-    <div class="__wrapper relative">
-        <h2 data-gsap-element="header" class="__before w-full md:w-1/2">{{ $g_logos['title'] }}</h2>
-    </div>
+	<div class="__wrapper relative">
+		<h2 data-gsap-element="header" class="__before w-full md:w-1/2">{{ $g_logos['title'] }}</h2>
+	</div>
 
-    @if (!empty($g_logos['logos_repeater']))
-    <div data-gsap-element="logos" class="__logos grid grid-cols-2 md:grid-cols-4 gap-4 mt-10">
-        @foreach ($g_logos['logos_repeater'] as $logo_item)
-        @php
-            $image = $logo_item['image'];
-            $link = $logo_item['link'];
-        @endphp
-        <div class="__logo relative border border-secondary-50 bg-white flex items-center justify-center h-48 p-8">
-            @if ($link)
-                <a href="{{ $link['url'] }}" target="{{ $link['target'] ? $link['target'] : '_blank' }}" rel="noopener noreferrer">
-            @endif
-            <img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="max-h-20 w-auto">
-            @if ($link)
-                </a>
-            @endif
-        </div>
-        @endforeach
-    </div>
-    @endif
+	@if (!empty($g_logos['gallery']))
+	<div data-gsap-element="logos" class="__logos grid grid-cols-2 md:grid-cols-4 gap-4 mt-10">
+		@foreach ($g_logos['gallery'] as $image)
+		<div class="__logo relative border b-border-light bg-white flex items-center justify-center h-30 p-8">
+			<img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="max-h-20 w-auto">
+		</div>
+		@endforeach
+	</div>
+	@endif
 
 </section>
